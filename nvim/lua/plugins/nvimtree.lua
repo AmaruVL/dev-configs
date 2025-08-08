0416516600d8561f7efa3d9fc8ecfaaaa046ae4d
AmaruVL <avillasanteleon@gmail.com>
2025-08-08 10:39:36 -0500

    Remove unused dependencies and clean up nvim-tree configuration

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -2,29 +2,11 @@ return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
   lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons", -- iconos opcionales
-  },
   config = function()
     require("nvim-tree").setup({
       view = {
         number = true,         -- número absoluto
-        relativenumber = true, -- numeración relativa para saltos
-      },
-      renderer = {
-        highlight_git = true,
-        icons = {
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-          },
-        },
-      },
-      git = {
-        enable = true,
-        ignore = false,
+        relativenumber = true, -- número relativo
       },
     })
 
