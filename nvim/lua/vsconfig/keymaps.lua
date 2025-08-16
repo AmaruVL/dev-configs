b0670c43902934b7e2b9f1ff0114c3d12fe42902
AmaruVL <avillasanteleon@gmail.com>
2025-08-16 11:36:04 -0500

    Gitignore actualizado

--- nvim/lua/vsconfig/keymaps.lua
+++ nvim/lua/vsconfig/keymaps.lua
@@ -9,6 +9,12 @@ keymap("n", "L", "<Cmd>Tabnext<CR>", opts)
 keymap("n", "H", "<Cmd>Tabprevious<CR>", opts)
 
 -- Acciones comunes
+keymap({ "v" }, "<C-c>", function() vscode("editor.action.clipboardCopyAction") end, opts) -- Copiar
+keymap({ "v" }, "<C-x>", function() vscode("editor.action.clipboardCutAction") end, opts) -- Cortar
+keymap({ "i" }, "<C-v>", function() vscode("editor.action.clipboardPasteAction") end, opts) -- Pegar (Solo en insert mode)
+
+keymap({ "n" }, "<C-w>b", function() vscode("editor.action.closeOtherEditors") end, opts) -- Cerrar buffers menos el actual
+
 keymap({ "n", "v" }, "<leader>t", function() vscode("workbench.action.terminal.toggleTerminal") end, opts)
 -- keymap({ "n", "v" }, "<leader>b", function() vscode("editor.debug.action.toggleBreakpoint") end, opts)
 keymap({ "n", "v" }, "<leader>d", function() vscode("editor.action.showHover") end, opts)
@@ -19,10 +25,10 @@ keymap({ "n", "v" }, "<leader>sp", function() vscode("workbench.actions.view.pro
 -- keymap({ "n", "v" }, "<leader>cp", function() vscode("workbench.action.showCommands") end, opts)
 -- keymap({ "n", "v" }, "<leader>pr", function() vscode("code-runner.run") end, opts)
 -- keymap({ "n", "v" }, "<leader>fd", function() vscode("editor.action.formatDocument") end, opts)
-keymap({ "n", "v" }, "<leader>h", function() vscode("workbench.action.focusLeftGroup") end, opts)
-keymap({ "n", "v" }, "<leader>j", function() vscode("workbench.action.focusBelowGroup") end, opts)
-keymap({ "n", "v" }, "<leader>k", function() vscode("workbench.action.focusAboveGroup") end, opts)
-keymap({ "n", "v" }, "<leader>l", function() vscode("workbench.action.focusRightGroup") end, opts)
+-- keymap({ "n", "v" }, "<leader>h", function() vscode("workbench.action.focusLeftGroup") end, opts)
+-- keymap({ "n", "v" }, "<leader>j", function() vscode("workbench.action.focusBelowGroup") end, opts)
+-- keymap({ "n", "v" }, "<leader>k", function() vscode("workbench.action.focusAboveGroup") end, opts)
+-- keymap({ "n", "v" }, "<leader>l", function() vscode("workbench.action.focusRightGroup") end, opts)
 keymap({ "v" }, "<", function() vscode("editor.action.outdentLines") end, opts)
 keymap({ "v" }, ">", function() vscode("editor.action.indentLines") end, opts)
 keymap({ "v" }, "J", function() vscode("editor.action.moveLinesDownAction") end, opts)
@@ -39,3 +45,4 @@ for i = 1, 9 do
     vscode("vscode-harpoon.gotoEditor" .. i)
   end, opts)
 end
+
