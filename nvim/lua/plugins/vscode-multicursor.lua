7c562c38454600461402aa008847aeef363fc3ab
AmaruVL <avillasanteleon@gmail.com>
2025-09-18 14:17:58 -0500

    Actualizacon

--- nvim/lua/plugins/vscode-multicursor.lua
+++ nvim/lua/plugins/vscode-multicursor.lua
@@ -13,6 +13,11 @@ return {
     require("vscode-multi-cursor").setup(opts)
 
     -- Tu keymap personalizado
-    vim.keymap.set("n", "<C-d>", "mciw*<Cmd>nohl<CR>", { remap = true, desc = "Seleccionar palabra y buscar siguiente" })
+    -- vim.keymap.set("n", "<C-d>", "mciw*<Cmd>nohl<CR>", { remap = true, desc = "Seleccionar palabra y buscar siguiente" })
+
+    vim.keymap.set({ "n", "x", "i" }, "<C-d>", function()
+      require("vscode-multi-cursor").addSelectionToNextFindMatch()
+    end)
+
   end,
 }
