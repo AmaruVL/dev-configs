976e81c15aa0c1b5320b8006ccff387cf02f8d8b
AmaruVL <avillasanteleon@gmail.com>
2025-09-18 12:51:44 -0500

    Update

--- nvim/lua/plugins/vscode-multicursor.lua
+++ nvim/lua/plugins/vscode-multicursor.lua
@@ -5,7 +5,14 @@ return {
     return vim.g.vscode -- solo cargar dentro de VSCode
   end,
   opts = {
-    default_mappings = true, -- activa los mapeos por defecto
-    no_selection = false,    -- crea cursores + selección múltiple
+    default_mappings = true,
+    no_selection = false,
   },
+  config = function(_, opts)
+    -- Inicializar el plugin con las opciones
+    require("vscode-multi-cursor").setup(opts)
+
+    -- Tu keymap personalizado
+    vim.keymap.set("n", "<C-d>", "mciw*<Cmd>nohl<CR>", { remap = true, desc = "Seleccionar palabra y buscar siguiente" })
+  end,
 }
