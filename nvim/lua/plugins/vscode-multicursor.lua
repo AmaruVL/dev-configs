477d13fcb9138eb988015252e86d664ff0a18383
AmaruVL <avillasanteleon@gmail.com>
2025-09-18 12:39:20 -0500

    Prueba

--- nvim/lua/plugins/vscode-multicursor.lua
+++ nvim/lua/plugins/vscode-multicursor.lua
@@ -12,22 +12,22 @@ return {
       remap = true, desc = "Add word selection & next (Normal)"
     })
 
-    -- -- Opcional: mapear <C-d> para "addSelectionToNextFindMatch" de VSCode via wrapper
-    -- vim.keymap.set({ "i" }, "<C-d>", function()
-    --   require("vscode-multi-cursor").addSelectionToNextFindMatch()
-    -- end, { desc = "Add next selection (VSCode Ctrl/⌘+D)" })
+    -- Opcional: mapear <C-d> para "addSelectionToNextFindMatch" de VSCode via wrapper
+    vim.keymap.set({ "i" }, "<C-d>", function()
+      require("vscode-multi-cursor").addSelectionToNextFindMatch()
+    end, { desc = "Add next selection (VSCode Ctrl/⌘+D)" })
 
-    -- -- Si quieres un “siguiente anterior” también:
-    -- vim.keymap.set({ "n", "x" }, "<C-S-d>", function()
-    --   require("vscode-multi-cursor").addSelectionToPreviousFindMatch()
-    -- end, { desc = "Add previous selection" })
+    -- Si quieres un “siguiente anterior” también:
+    vim.keymap.set({ "n", "x" }, "<C-S-d>", function()
+      require("vscode-multi-cursor").addSelectionToPreviousFindMatch()
+    end, { desc = "Add previous selection" })
 
-    -- -- Si usas flash.nvim, ya tienes por defecto:
-    -- --  mcs (char) y mcw (word). No necesitas hacer nada más.
+    -- Si usas flash.nvim, ya tienes por defecto:
+    --  mcs (char) y mcw (word). No necesitas hacer nada más.
   end,
-  -- keys = {
-  --   -- -- (Opcional) atajo explícito por si quieres tenerlo visible en :map
-  --   -- { "<C-d>", mode = { "n", "x", "i" }, desc = "VSCode addSelectionToNextFindMatch" },
-  --   -- { "<C-S-d>", mode = { "n", "x" }, desc = "VSCode addSelectionToPreviousFindMatch" },
-  -- },
+  keys = {
+    -- -- (Opcional) atajo explícito por si quieres tenerlo visible en :map
+    -- { "<C-d>", mode = { "n", "x", "i" }, desc = "VSCode addSelectionToNextFindMatch" },
+    -- { "<C-S-d>", mode = { "n", "x" }, desc = "VSCode addSelectionToPreviousFindMatch" },
+  },
 }
